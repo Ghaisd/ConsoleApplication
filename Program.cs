52df2d2b8b04cf242967c717bcaec9352a37e002
Ghias Dahdouh <ghaisdahdouh@gmail.com>
2022-03-14 15:47:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿while (true)
+{
+    Console.WriteLine("Enter the year (Enter n to stop)");
+    string? s = Console.ReadLine();
+    if (s == "n" || s == "N")
+    {
+        return;
+    }
+    else if (Int32.TryParse(s, out int year))
+    {
+        LeapYear(year);
+    }
+}
+
+void LeapYear(int year)
+{
+    if (year % 400 == 0)
+    {
+        Console.WriteLine("It is a leap year");
+    }
+    else if (year % 100 == 0)
+    {
+        Console.WriteLine("It is not a leap year");
+    }
+    else if (year % 4 == 0)
+    {
+        Console.WriteLine("It is a leap year");
+    }
+    else
+    {
+        Console.WriteLine("It is not a leap year");
+    }
+
+}
